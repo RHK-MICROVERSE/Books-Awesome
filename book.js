a3d2612f589fc28dbae4009dcbd4eaba1e3a9d0e
Fickry Iman <fickry.iman@gmail.com>
2023-03-22 13:01:28 +0700

    create methods on class

--- book.js
+++ book.js
@@ -1,112 +1,93 @@
 /* Checking local storage if undefined will creeate an empty array */
-let bookCollection = JSON.parse(localStorage.getItem('BOOKS')) || [];
+class Book {
+  constructor(title, author, index = null) {
+    this.title = title;
+    this.author = author;
+    this.index = index;
+  }
+}
 
-// Get Document references to manupulate
-const bookList = document.getElementById('book-list');
+class BookCRUD {
+  static readBook() {
+    let booksCollection = localStorage.getItem('BOOKS') === null ? [] : JSON.parse(localStorage.getItem('BOOKS'));
+    return booksCollection;
+  }
+
+  static createBook(book) {
+    let booksCollection = BookCRUD.readBook();
+    booksCollection.push(book);
+    localStorage.setItem('BOOKS', JSON.stringify(booksCollection));
+  }
+
+  static removeBook(index) {
+    let booksCollection = BookCRUD.readBook();
+    booksCollection = booksCollection.filter((book, ref) => ref !== +index);
+    localStorage.setItem('BOOKS', JSON.stringify(booksCollection));
+  }
+}
+
+class Interface {
+  static renderBooks() {
+    let bookList = document.getElementById('book-list');
+    let booksCollection = BookCRUD.readBook();
+    bookList.innerHTML = booksCollection.map((book, ref) => (ref % 2 === 0 ? ` 
+                            <div class="bookslist-container grey">
+                              <div class="book-info">
+                                <li>
+                                  "${book.title}" by
+                                </li>
+                                <li>
+                                  ${book.author}
+                                </li>
+                              </div>
+                              <button class='rm-btn' 
+                              id="remove-btn"
+                              data-index='${ref}'>Remove
+                              </button>        
+                            </div>
+                          ` : `
+                            <div class="bookslist-container">
+                              <div class="book-info">
+                                <li>
+                                  "${book.title}" by
+                                </li>
+                                <li>
+                                  ${book.author}
+                                </li>
+                              </div>
+                              <button class='rm-btn' 
+                              id="remove-btn"
+                              data-index='${ref}'>Remove
+                              </button>        
+                            </div>
+                          `)).join('')
+  }
+}
+
+document.addEventListener('DOMContentLoaded', Interface.renderBooks);
+
 const addForm = document.getElementById('add-form');
 const titleInput = document.getElementById('book-title');
 const authorInput = document.getElementById('book-author');
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-
-  addBook() {
-    let book = {}
-    book.title = this.title;
-    book.author = this.author;
-    
-    if (localStorage.getItem('BOOKS') === null) {
-      localStorage.setItem('BOOKS', []);
-      bookCollection.push(book);
-      localStorage.setItem('BOOKS', JSON.stringify(bookCollection));
-    } else {
-      bookCollection.push(book);
-      localStorage.setItem('BOOKS', JSON.stringify(bookCollection));
-    }
-  }
-
-  removeBook(index) {
-    console.log('trying to remove book');
-    bookCollection = bookCollection.filter((book, ref) => ref !== +index);
-    localStorage.setItem('BOOKS', JSON.stringify(bookCollection));
-  }
-}
-
-// Function to render the book list
-function renderBookList() {
-  bookList.innerHTML = bookCollection
-    .map(
-      (book, ref) => ref % 2 === 0 ? ` 
-          <div class="bookslist-container grey">
-            <div class="book-info">
-              <li>
-                "${book.title}" by
-              </li>
-              <li>
-                ${book.author}
-              </li>
-            </div>
-            <button class='rm-btn' 
-            id="remove-btn"
-            data-index='${ref}'>Remove
-            </button>        
-          </div>
-        ` : `
-          <div class="bookslist-container">
-            <div class="book-info">
-              <li>
-                "${book.title}" by
-              </li>
-              <li>
-                ${book.author}
-              </li>
-            </div>
-            <button class='rm-btn' 
-            id="remove-btn"
-            data-index='${ref}'>Remove
-            </button>        
-          </div>
-        `,
-    )
-    .join('');
-}
-
-// Declaring Rendering of the Book List
-renderBookList();
-
-/* EVENT LISTENER ONE: add book SUBMIT
-Add a new book to the collection when clicked submit
-- Confirm the title and Authour value exists
-- Call the addBook Function to add book in BookCollection
-- Call the renderBookList function to refresh the BookList
-- Reset the Form for new book addition */
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = titleInput.value;
-  const author = authorInput.value;
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let book = new Book(title, author);
   if (title && author) {
-    // Function to add title and author, refresh Booklist.
-    let newBook = new Book(title, author);
-    newBook.addBook()
-    // addBook(title, author);
-    renderBookList();
+    BookCRUD.createBook(book);
+    Interface.renderBooks();
     addForm.reset();
   }
 });
 
-/* EVENT LISTENR TWO: click REMOVE
-- Call the removeBook Function to rmove book from BookCollection
-- Call the renderBookList function to refresh the BookList */
+const bookList = document.getElementById('book-list');
+
 bookList.addEventListener('click', (e) => {
   if (e.target.matches('.rm-btn')) {
     const bookIndex = e.target.dataset.index;
-    // A function to remove book
-    let newBook = new Book();
-    newBook.removeBook(bookIndex);
-    // removeBook(bookIndex);
-    renderBookList();
+    BookCRUD.removeBook(bookIndex);
+    Interface.renderBooks();
   }
 });
